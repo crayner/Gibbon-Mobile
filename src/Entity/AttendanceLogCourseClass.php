44ce9a2675bce09e9f76a0af7d758cf4fd908368
Craig Rayner <craig@craigrayner.com>
2018-12-01 08:14:22 +1100

    App\Entity\AttendanceLogCourseClass Entity added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/AttendanceLogCourseClass.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class AttendanceLogCourseClass
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\AttendanceLogCourseClassRepository")
+ * @ORM\Table(name="AttendanceLogCourseClass")
+ */
+class AttendanceLogCourseClass
+{
+    /**
+     * @var integer|null
+     * @ORM\Id()
+     * @ORM\Column(type="smallint", name="gibbonAttendanceLogCourseClassID", columnDefinition="INT(3) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="CourseClass")
+     * @ORM\JoinColumn(name="gibbonCourseClassID", referencedColumnName="gibbonCourseClassID")
+     */
+    private $courseClass;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDTaker", referencedColumnName="gibbonPersonID")
+     */
+    private $person;
+
+    /**
+     * @var \DateTime|null
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var \DateTime|null
+     * @ORM\Column(type="datetime", name="timestampTaken")
+     */
+    private $timestampTaken;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return AttendanceLogCourseClass
+     */
+    public function setId(?int $id): AttendanceLogCourseClass
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getCourseClass(): ?Person
+    {
+        return $this->courseClass;
+    }
+
+    /**
+     * @param Person|null $courseClass
+     * @return AttendanceLogCourseClass
+     */
+    public function setCourseClass(?Person $courseClass): AttendanceLogCourseClass
+    {
+        $this->courseClass = $courseClass;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person|null $person
+     * @return AttendanceLogCourseClass
+     */
+    public function setPerson(?Person $person): AttendanceLogCourseClass
+    {
+        $this->person = $person;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime|null $date
+     * @return AttendanceLogCourseClass
+     */
+    public function setDate(?\DateTime $date): AttendanceLogCourseClass
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimestampTaken(): ?\DateTime
+    {
+        return $this->timestampTaken;
+    }
+
+    /**
+     * @param \DateTime|null $timestampTaken
+     * @return AttendanceLogCourseClass
+     */
+    public function setTimestampTaken(?\DateTime $timestampTaken): AttendanceLogCourseClass
+    {
+        $this->timestampTaken = $timestampTaken;
+        return $this;
+    }
+
+    /**
+     * updateTakenTime
+     * @return AttendanceLogCourseClass
+     * @throws \Exception
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function updateTakenTime()
+    {
+        return $this->setTimestampTaken(new \DateTime('now'));
+    }
+}
