47832543c04a92f8f6049b02338d10fe1cb06f25
Craig Rayner <craig@craigrayner.com>
2018-12-02 08:43:31 +1100

    DepartmentStaff Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/DepartmentStaff.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class DepartmentStaff
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\DepartmentStaffRepository")
+ * @ORM\Table(name="DepartmentStaff")
+ */
+class DepartmentStaff
+{
+    /**
+     * @var integer|null
+     * @ORM\Id()
+     * @ORM\Column(type="smallint", name="gibbonDepartmentStaffID", columnDefinition="INT(4) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var Department|null
+     * @ORM\ManyToOne(targetEntity="Department")
+     * @ORM\JoinColumn(name="gibbonDepartmentID", referencedColumnName="gibbonDepartmentID")
+     */
+    private $department;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonID", referencedColumnName="gibbonPersonID")
+     */
+    private $person;
+
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=24)
+     */
+    private $role;
+
+    /**
+     * @var array
+     */
+    private static $roleList = ['Coordinator','Assistant Coordinator','Teacher (Curriculum)','Teacher','Director','Manager','Administrator','Other'];
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return DepartmentStaff
+     */
+    public function setId(?int $id): DepartmentStaff
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Department|null
+     */
+    public function getDepartment(): ?Department
+    {
+        return $this->department;
+    }
+
+    /**
+     * @param Department|null $department
+     * @return DepartmentStaff
+     */
+    public function setDepartment(?Department $department): DepartmentStaff
+    {
+        $this->department = $department;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person|null $person
+     * @return DepartmentStaff
+     */
+    public function setPerson(?Person $person): DepartmentStaff
+    {
+        $this->person = $person;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string|null $role
+     * @return DepartmentStaff
+     */
+    public function setRole(?string $role): DepartmentStaff
+    {
+        $this->role = in_array($role, self::getRoleList()) ? $role : '';
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoleList(): array
+    {
+        return self::$roleList;
+    }
+}
