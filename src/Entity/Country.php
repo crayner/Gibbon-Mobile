af2ce4793ef192cc0eec942fe25464460d509787
Craig Rayner <craig@craigrayner.com>
2018-12-01 20:29:09 +1100

    Country Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/Country.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Country
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\CountryRepository")
+ * @ORM\Table(name="Country")
+ */
+class Country
+{
+    /**
+     * @var string|null
+     * @ORM\Column(length=80)
+     */
+    private $printable_name;
+
+    /**
+     * @var string|null
+     * @ORM\Id()
+     * @ORM\Column(length=7, name="iddCountryCode")
+     */
+    private $iddCountryCode;
+}
