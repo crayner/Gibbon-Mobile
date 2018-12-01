edb6c5a3154d6c39c9e75f485f675e12569e0dd3
Craig Rayner <craig@craigrayner.com>
2018-12-02 07:33:52 +1100

    CourseClassPerson Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/CourseClassPerson.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use App\Manager\Traits\BooleanList;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class CourseClassPerson
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\CourseClassPersonRepository")
+ * @ORM\Table(name="CourseClassPerson", indexes={@ORM\Index(name="gibbonCourseClassID", columns={"gibbonCourseClassID"}), @ORM\Index(name="gibbonPersonID", columns={"gibbonPersonID", "role"})})
+ */
+class CourseClassPerson
+{
+    use BooleanList;
+    /**
+     * @var integer|null
+     * @ORM\Id()
+     * @ORM\Column(type="integer", name="gibbonCourseClassPersonID", columnDefinition="INT(10) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var CourseClass|null
+     * @ORM\ManyToOne(targetEntity="CourseClass")
+     * @ORM\JoinColumn(name="gibbonCourseClassID", referencedColumnName="gibbonCourseClassID")
+     */
+    private $courseClass;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonID", referencedColumnName="gibbonPersonID")
+     */
+    private $person;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=16)
+     */
+    private $role = '';
+
+    /**
+     * @var array
+     */
+    private static $roleList = ['Student','Teacher','Assistant','Technician','Parent','Student - Left','Teacher - Left'];
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $reportable = 'Y';
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return CourseClassPerson
+     */
+    public function setId(?int $id): CourseClassPerson
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return CourseClass|null
+     */
+    public function getCourseClass(): ?CourseClass
+    {
+        return $this->courseClass;
+    }
+
+    /**
+     * @param CourseClass|null $courseClass
+     * @return CourseClassPerson
+     */
+    public function setCourseClass(?CourseClass $courseClass): CourseClassPerson
+    {
+        $this->courseClass = $courseClass;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person|null $person
+     * @return CourseClassPerson
+     */
+    public function setPerson(?Person $person): CourseClassPerson
+    {
+        $this->person = $person;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string|null $role
+     * @return CourseClassPerson
+     */
+    public function setRole(?string $role): CourseClassPerson
+    {
+        $this->role = in_array($role, self::getRoleList()) ? $role : '';
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReportable(): ?string
+    {
+        return $this->reportable;
+    }
+
+    /**
+     * @param string|null $reportable
+     * @return CourseClassPerson
+     */
+    public function setReportable(?string $reportable): CourseClassPerson
+    {
+        $this->reportable = self::checkBoolean($reportable, 'Y');
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoleList(): array
+    {
+        return self::$roleList;
+    }
+}
