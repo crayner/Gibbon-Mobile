b318a719bc5cc0f10e8e4fe764520a1de7d23c20
Craig Rayner <craig@craigrayner.com>
2018-12-02 15:55:36 +1100

    Family Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Entity/Family.php
+++ src/Entity/Family.php
@@ -47,4 +47,251 @@ class Family
      * @ORM\GeneratedValue
      */
     private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=100)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=100, name="nameAddress")
+     */
+    private $nameAddress;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text", name="homeAddress")
+     */
+    private $homeAddress;
+
+    /**
+     * @var string|null
+     * @ORM\Column(name="homeAddressDistrict")
+     */
+    private $homeAddressDistrict;
+
+    /**
+     * @var string|null
+     * @ORM\Column(name="homeAddressCountry")
+     */
+    private $homeAddressCountry;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=12)
+     */
+    private $status = 'Unknown';
+
+    /**
+     * @var array
+     */
+    private static $statusList = ['Married','Separated','Divorced','De Facto','Other'];
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=30, name="languageHomePrimary")
+     */
+    private $languageHomePrimary;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=30, name="languageHomeSecondary")
+     */
+    private $languageHomeSecondary;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=50, name="familySync")
+     */
+    private $familySync;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return Family
+     */
+    public function setId(?int $id): Family
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return Family
+     */
+    public function setName(?string $name): Family
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNameAddress(): ?string
+    {
+        return $this->nameAddress;
+    }
+
+    /**
+     * @param string|null $nameAddress
+     * @return Family
+     */
+    public function setNameAddress(?string $nameAddress): Family
+    {
+        $this->nameAddress = $nameAddress;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getHomeAddress(): ?string
+    {
+        return $this->homeAddress;
+    }
+
+    /**
+     * @param string|null $homeAddress
+     * @return Family
+     */
+    public function setHomeAddress(?string $homeAddress): Family
+    {
+        $this->homeAddress = $homeAddress;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getHomeAddressDistrict(): ?string
+    {
+        return $this->homeAddressDistrict;
+    }
+
+    /**
+     * @param string|null $homeAddressDistrict
+     * @return Family
+     */
+    public function setHomeAddressDistrict(?string $homeAddressDistrict): Family
+    {
+        $this->homeAddressDistrict = $homeAddressDistrict;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getHomeAddressCountry(): ?string
+    {
+        return $this->homeAddressCountry;
+    }
+
+    /**
+     * @param string|null $homeAddressCountry
+     * @return Family
+     */
+    public function setHomeAddressCountry(?string $homeAddressCountry): Family
+    {
+        $this->homeAddressCountry = $homeAddressCountry;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string|null $status
+     * @return Family
+     */
+    public function setStatus(?string $status): Family
+    {
+        $this->status = in_array($status, self::getStatusList()) ? $status : 'Unknown';
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLanguageHomePrimary(): ?string
+    {
+        return $this->languageHomePrimary;
+    }
+
+    /**
+     * @param string|null $languageHomePrimary
+     * @return Family
+     */
+    public function setLanguageHomePrimary(?string $languageHomePrimary): Family
+    {
+        $this->languageHomePrimary = $languageHomePrimary;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLanguageHomeSecondary(): ?string
+    {
+        return $this->languageHomeSecondary;
+    }
+
+    /**
+     * @param string|null $languageHomeSecondary
+     * @return Family
+     */
+    public function setLanguageHomeSecondary(?string $languageHomeSecondary): Family
+    {
+        $this->languageHomeSecondary = $languageHomeSecondary;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFamilySync(): ?string
+    {
+        return $this->familySync;
+    }
+
+    /**
+     * @param string|null $familySync
+     * @return Family
+     */
+    public function setFamilySync(?string $familySync): Family
+    {
+        $this->familySync = $familySync;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getStatusList(): array
+    {
+        return self::$statusList;
+    }
 }
