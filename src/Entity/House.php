f5625c387d6b76ef71d15564294c7b5330254986
Craig Rayner <craig@craigrayner.com>
2018-12-04 10:47:16 +1100

    House Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Entity/House.php
+++ src/Entity/House.php
@@ -38,17 +38,35 @@ use Doctrine\ORM\Mapping as ORM;
  * Class House
  * @package App\Entity
  * @ORM\Entity(repositoryClass="App\Repository\HouseRepository")
- * @ORM\Table(name="House")
+ * @ORM\Table(name="House", uniqueConstraints={@ORM\UniqueConstraint(name="name", columns={"name","nameShort"})})
  */
 class House
 {
     /**
      * @var integer|null
      * @ORM\Id
-     * @ORM\Column(type="integer", name="gibbonHouseID", columnDefinition="INT(3) UNSIGNED ZEROFILL")
+     * @ORM\Column(type="smallint", name="gibbonHouseID", columnDefinition="INT(3) UNSIGNED ZEROFILL")
      * @ORM\GeneratedValue
      */
     private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=30)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=10, name="nameShort")
+     */
+    private $nameShort;
+
+    /**
+     * @var string|null
+     * @ORM\Column()
+     */
+    private $logo;
 
     /**
      * @return int|null
@@ -65,6 +83,60 @@ class House
     public function setId(?int $id): House
     {
         $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return House
+     */
+    public function setName(?string $name): House
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNameShort(): ?string
+    {
+        return $this->nameShort;
+    }
+
+    /**
+     * @param string|null $nameShort
+     * @return House
+     */
+    public function setNameShort(?string $nameShort): House
+    {
+        $this->nameShort = $nameShort;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    /**
+     * @param string|null $logo
+     * @return House
+     */
+    public function setLogo(?string $logo): House
+    {
+        $this->logo = $logo;
         return $this;
     }
 }
