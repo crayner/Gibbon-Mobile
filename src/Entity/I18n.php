3fb8757e2267adf029bdd2d4e5044acf36238e0a
Craig Rayner <craig@craigrayner.com>
2018-12-04 10:53:51 +1100

    i18n Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Entity/I18n.php
+++ src/Entity/I18n.php
@@ -28,6 +28,7 @@
  */
 namespace App\Entity;
 
+use App\Manager\Traits\BooleanList;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -38,6 +39,8 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class I18n
 {
+    use BooleanList;
+
     /**
      * @var integer|null
      * @ORM\Id
@@ -45,6 +48,66 @@ class I18n
      * @ORM\GeneratedValue
      */
     private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=5)
+     */
+    private $code;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=100)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=10, nullable=true)
+     */
+    private $version;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $active = 'Y';
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $installed = 'N';
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1, name="systemDefault")
+     */
+    private $systemDefault = 'N';
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=20, name="dateFormat")
+     */
+    private $dateFormat;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text", name="dateFormatRegEx")
+     */
+    private $dateFormatRegEx;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=20, name="dateFormatPHP")
+     */
+    private $dateFormatPHP;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $rtl = 'N';
 
     /**
      * @return int|null
@@ -65,12 +128,6 @@ class I18n
     }
 
     /**
-     * @var string|null
-     * @ORM\Column(length=5, nullable=true)
-     */
-    private $code;
-
-    /**
      * @return string|null
      */
     public function getCode(): ?string
@@ -79,22 +136,14 @@ class I18n
     }
 
     /**
-     * setCode
-     *
      * @param string|null $code
      * @return I18n
      */
     public function setCode(?string $code): I18n
     {
-        $this->code = mb_substr($code, 0, 5);
+        $this->code = $code;
         return $this;
     }
-
-    /**
-     * @var string|null
-     * @ORM\Column(length=100, nullable=true)
-     */
-    private $name;
 
     /**
      * @return string|null
@@ -105,14 +154,156 @@ class I18n
     }
 
     /**
-     * setName
-     *
      * @param string|null $name
      * @return I18n
      */
     public function setName(?string $name): I18n
     {
-        $this->name = mb_substr($name, 0, 100);
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param string|null $version
+     * @return I18n
+     */
+    public function setVersion(?string $version): I18n
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getActive(): ?string
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param string|null $active
+     * @return I18n
+     */
+    public function setActive(?string $active): I18n
+    {
+        $this->active = self::checkBoolean($active, 'Y');
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getInstalled(): ?string
+    {
+        return $this->installed;
+    }
+
+    /**
+     * @param string|null $installed
+     * @return I18n
+     */
+    public function setInstalled(?string $installed): I18n
+    {
+        $this->installed = self::checkBoolean($installed, 'N');
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSystemDefault(): ?string
+    {
+        return $this->systemDefault;
+    }
+
+    /**
+     * @param string|null $systemDefault
+     * @return I18n
+     */
+    public function setSystemDefault(?string $systemDefault): I18n
+    {
+        $this->systemDefault = self::checkBoolean($systemDefault, 'N');
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDateFormat(): ?string
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * @param string|null $dateFormat
+     * @return I18n
+     */
+    public function setDateFormat(?string $dateFormat): I18n
+    {
+        $this->dateFormat = $dateFormat;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDateFormatRegEx(): ?string
+    {
+        return $this->dateFormatRegEx;
+    }
+
+    /**
+     * @param string|null $dateFormatRegEx
+     * @return I18n
+     */
+    public function setDateFormatRegEx(?string $dateFormatRegEx): I18n
+    {
+        $this->dateFormatRegEx = $dateFormatRegEx;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDateFormatPHP(): ?string
+    {
+        return $this->dateFormatPHP;
+    }
+
+    /**
+     * @param string|null $dateFormatPHP
+     * @return I18n
+     */
+    public function setDateFormatPHP(?string $dateFormatPHP): I18n
+    {
+        $this->dateFormatPHP = $dateFormatPHP;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRtl(): ?string
+    {
+        return $this->rtl;
+    }
+
+    /**
+     * @param string|null $rtl
+     * @return I18n
+     */
+    public function setRtl(?string $rtl): I18n
+    {
+        $this->rtl = self::checkBoolean($rtl, 'N');
         return $this;
     }
 }
