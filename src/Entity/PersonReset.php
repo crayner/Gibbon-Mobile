843e193fd8d87211941f6dd8fde254ba51faf919
Craig Rayner <craig@craigrayner.com>
2018-12-04 18:25:24 +1100

    PersonReset Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/PersonReset.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class PersonReset
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\PersonResetRepository")
+ * @ORM\Table(name="PersonReset")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class PersonReset
+{
+    /**
+     * @var integer|null
+     * @ORM\Id
+     * @ORM\Column(type="bigint", name="gibbonPersonResetID", columnDefinition="INT(12) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonID", referencedColumnName="gibbonPersonID")
+     */
+    private $person;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=40)
+     */
+    private $key;
+
+    /**
+     * @var \DateTime|null
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $timestamp;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return PersonReset
+     */
+    public function setId(?int $id): PersonReset
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person|null $person
+     * @return PersonReset
+     */
+    public function setPerson(?Person $person): PersonReset
+    {
+        $this->person = $person;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string|null $key
+     * @return PersonReset
+     */
+    public function setKey(?string $key): PersonReset
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimestamp(): ?\DateTime
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * setTimestamp
+     * @param \DateTime|null $timestamp
+     * @return PersonReset
+     * @throws \Exception
+     * @ORM\PrePersist()
+     */
+    public function setTimestamp(?\DateTime $timestamp = null): PersonReset
+    {
+        $this->timestamp = $timestamp ?: new \DateTime('now');
+        return $this;
+    }
+}
