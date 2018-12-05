6a9890b26ad4707024e19dceb3dfe2d59a1da078
Craig Rayner <craig@craigrayner.com>
2018-12-05 12:00:28 +1100

    PlannerEntryOutcome Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/PlannerEntryOutcome.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 23/11/2018
+ * Time: 15:27
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class PlannerEntryOutcome
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\PlannerEntryOutcomeRepository")
+ * @ORM\Table(name="PlannerEntryOutcome")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class PlannerEntryOutcome
+{
+    /**
+     * @var integer|null
+     * @ORM\Id()
+     * @ORM\Column(type="bigint", name="gibbonPlannerEntryOutcomeID", columnDefinition="INT(16) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var PlannerEntry|null
+     * @ORM\ManyToOne(targetEntity="PlannerEntry")
+     * @ORM\JoinColumn(name="gibbonPlannerEntryID", referencedColumnName="gibbonPlannerEntryID")
+     */
+    private $plannerEntry;
+
+    /**
+     * @var Outcome|null
+     * @ORM\ManyToOne(targetEntity="Outcome")
+     * @ORM\JoinColumn(name="gibbonOutcomeID", referencedColumnName="gibbonOutcomeID")
+     */
+    private $outcome;
+
+    /**
+     * @var integer|null
+     * @ORM\Column(type="smallint", name="sequenceNumber", columnDefinition="INT(4)")
+     */
+    private $sequenceNumber;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return PlannerEntryOutcome
+     */
+    public function setId(?int $id): PlannerEntryOutcome
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return PlannerEntry|null
+     */
+    public function getPlannerEntry(): ?PlannerEntry
+    {
+        return $this->plannerEntry;
+    }
+
+    /**
+     * @param PlannerEntry|null $plannerEntry
+     * @return PlannerEntryOutcome
+     */
+    public function setPlannerEntry(?PlannerEntry $plannerEntry): PlannerEntryOutcome
+    {
+        $this->plannerEntry = $plannerEntry;
+        return $this;
+    }
+
+    /**
+     * @return Outcome|null
+     */
+    public function getOutcome(): ?Outcome
+    {
+        return $this->outcome;
+    }
+
+    /**
+     * @param Outcome|null $outcome
+     * @return PlannerEntryOutcome
+     */
+    public function setOutcome(?Outcome $outcome): PlannerEntryOutcome
+    {
+        $this->outcome = $outcome;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getSequenceNumber(): ?int
+    {
+        return $this->sequenceNumber;
+    }
+
+    /**
+     * @param int|null $sequenceNumber
+     * @return PlannerEntryOutcome
+     */
+    public function setSequenceNumber(?int $sequenceNumber): PlannerEntryOutcome
+    {
+        $this->sequenceNumber = $sequenceNumber;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string|null $content
+     * @return PlannerEntryOutcome
+     */
+    public function setContent(?string $content): PlannerEntryOutcome
+    {
+        $this->content = $content;
+        return $this;
+    }
+}
