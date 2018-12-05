4db1f2925b907d7b4f7bc37e0a346a29575fab2a
Craig Rayner <craig@craigrayner.com>
2018-12-05 13:18:50 +1100

    RollGroup Entity Tweaked
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Entity/RollGroup.php
+++ src/Entity/RollGroup.php
@@ -29,6 +29,7 @@
  */
 namespace App\Entity;
 
+use App\Manager\Traits\BooleanList;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -39,6 +40,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class RollGroup
 {
+    use BooleanList;
     /**
      * @var integer|null
      * @ORM\Id()
@@ -46,4 +48,343 @@ class RollGroup
      * @ORM\GeneratedValue
      */
     private $id;
+
+    /**
+     * @var SchoolYear|null
+     * @ORM\ManyToOne(targetEntity="SchoolYear")
+     * @ORM\JoinColumn(name="gibbonSchoolYearID", referencedColumnName="gibbonSchoolYearID")
+     */
+    private $schoolYear;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=10)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=5, name="nameShort")
+     */
+    private $nameShort;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDTutoe",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $tutor;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDTutoe2",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $tutor2;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDTutor3",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $tutoe3;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDEA",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $assistant;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDEA2",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $assistant2;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDEA3",referencedColumnName="gibbonPersonID", nullable=true)
+     */
+    private $assistant3;
+
+    /**
+     * @var Space|null
+     * @ORM\ManyToOne(targetEntity="Space")
+     * @ORM\JoinColumn(name="gibbonSpaceID", referencedColumnName="gibbonSpaceID", nullable=true)
+     */
+    private $space;
+
+    /**
+     * @var RollGroup|null
+     * @ORM\ManyToOne(targetEntity="RollGroup")
+     * @ORM\JoinColumn(name="gibbonRollGroupIDNext", referencedColumnName="gibbonRollGroupID", nullable=true)
+     */
+    private $nextRollGroup;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $attendance = 'Y';
+
+    /**
+     * @var string|null
+     * @ORM\Column()
+     */
+    private $website;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return RollGroup
+     */
+    public function setId(?int $id): RollGroup
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return SchoolYear|null
+     */
+    public function getSchoolYear(): ?SchoolYear
+    {
+        return $this->schoolYear;
+    }
+
+    /**
+     * @param SchoolYear|null $schoolYear
+     * @return RollGroup
+     */
+    public function setSchoolYear(?SchoolYear $schoolYear): RollGroup
+    {
+        $this->schoolYear = $schoolYear;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return RollGroup
+     */
+    public function setName(?string $name): RollGroup
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNameShort(): ?string
+    {
+        return $this->nameShort;
+    }
+
+    /**
+     * @param string|null $nameShort
+     * @return RollGroup
+     */
+    public function setNameShort(?string $nameShort): RollGroup
+    {
+        $this->nameShort = $nameShort;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getTutor(): ?Person
+    {
+        return $this->tutor;
+    }
+
+    /**
+     * @param Person|null $tutor
+     * @return RollGroup
+     */
+    public function setTutor(?Person $tutor): RollGroup
+    {
+        $this->tutor = $tutor;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getTutor2(): ?Person
+    {
+        return $this->tutor2;
+    }
+
+    /**
+     * @param Person|null $tutor2
+     * @return RollGroup
+     */
+    public function setTutor2(?Person $tutor2): RollGroup
+    {
+        $this->tutor2 = $tutor2;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getTutoe3(): ?Person
+    {
+        return $this->tutoe3;
+    }
+
+    /**
+     * @param Person|null $tutoe3
+     * @return RollGroup
+     */
+    public function setTutoe3(?Person $tutoe3): RollGroup
+    {
+        $this->tutoe3 = $tutoe3;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getAssistant(): ?Person
+    {
+        return $this->assistant;
+    }
+
+    /**
+     * @param Person|null $assistant
+     * @return RollGroup
+     */
+    public function setAssistant(?Person $assistant): RollGroup
+    {
+        $this->assistant = $assistant;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getAssistant2(): ?Person
+    {
+        return $this->assistant2;
+    }
+
+    /**
+     * @param Person|null $assistant2
+     * @return RollGroup
+     */
+    public function setAssistant2(?Person $assistant2): RollGroup
+    {
+        $this->assistant2 = $assistant2;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getAssistant3(): ?Person
+    {
+        return $this->assistant3;
+    }
+
+    /**
+     * @param Person|null $assistant3
+     * @return RollGroup
+     */
+    public function setAssistant3(?Person $assistant3): RollGroup
+    {
+        $this->assistant3 = $assistant3;
+        return $this;
+    }
+
+    /**
+     * @return Space|null
+     */
+    public function getSpace(): ?Space
+    {
+        return $this->space;
+    }
+
+    /**
+     * @param Space|null $space
+     * @return RollGroup
+     */
+    public function setSpace(?Space $space): RollGroup
+    {
+        $this->space = $space;
+        return $this;
+    }
+
+    /**
+     * @return RollGroup|null
+     */
+    public function getNextRollGroup(): ?RollGroup
+    {
+        return $this->nextRollGroup;
+    }
+
+    /**
+     * @param RollGroup|null $nextRollGroup
+     * @return RollGroup
+     */
+    public function setNextRollGroup(?RollGroup $nextRollGroup): RollGroup
+    {
+        $this->nextRollGroup = $nextRollGroup;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAttendance(): ?string
+    {
+        return $this->attendance;
+    }
+
+    /**
+     * @param string|null $attendance
+     * @return RollGroup
+     */
+    public function setAttendance(?string $attendance): RollGroup
+    {
+        $this->attendance = self::checkBoolean($attendance);
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    /**
+     * @param string|null $website
+     * @return RollGroup
+     */
+    public function setWebsite(?string $website): RollGroup
+    {
+        $this->website = $website;
+        return $this;
+    }
 }
