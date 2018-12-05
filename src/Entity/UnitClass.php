0c5924240805467687baa176ea179ef82e96b048
Craig Rayner <craig@craigrayner.com>
2018-12-05 22:08:43 +1100

    UnitClass Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/UnitClass.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 5/12/2018
+ * Time: 22:05
+ */
+namespace App\Entity;
+
+use App\Manager\Traits\BooleanList;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class UnitClass
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\UnitClassRepository")
+ * @ORM\Table(name="UnitClass")
+ */
+class UnitClass
+{
+    use BooleanList;
+
+    /**
+     * @var integer|null
+     * @ORM\Id
+     * @ORM\Column(type="bigint", name="gibbonUnitClassID", columnDefinition="INT(12) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var Unit|null
+     * @ORM\ManyToOne(targetEntity="Unit")
+     * @ORM\JoinColumn(name="gibbonUnitID", referencedColumnName="gibbonUnitID")
+     */
+    private $unit;
+
+    /**
+     * @var CourseClass|null
+     * @ORM\ManyToOne(targetEntity="CourseClass")
+     * @ORM\JoinColumn(name="gibbonCourseClassID", referencedColumnName="gibbonCourseClassID")
+     */
+    private $courseClass;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=1)
+     */
+    private $running = 'N';
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return UnitClass
+     */
+    public function setId(?int $id): UnitClass
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Unit|null
+     */
+    public function getUnit(): ?Unit
+    {
+        return $this->unit;
+    }
+
+    /**
+     * @param Unit|null $unit
+     * @return UnitClass
+     */
+    public function setUnit(?Unit $unit): UnitClass
+    {
+        $this->unit = $unit;
+        return $this;
+    }
+
+    /**
+     * @return CourseClass|null
+     */
+    public function getCourseClass(): ?CourseClass
+    {
+        return $this->courseClass;
+    }
+
+    /**
+     * @param CourseClass|null $courseClass
+     * @return UnitClass
+     */
+    public function setCourseClass(?CourseClass $courseClass): UnitClass
+    {
+        $this->courseClass = $courseClass;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRunning(): ?string
+    {
+        return $this->running;
+    }
+
+    /**
+     * @param string|null $running
+     * @return UnitClass
+     */
+    public function setRunning(?string $running): UnitClass
+    {
+        $this->running = self::checkBoolean($running, 'N');
+        return $this;
+    }
+}
