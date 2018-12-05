1fd59ccfdd6b7ec4150045018710a9ea97281cfc
Craig Rayner <craig@craigrayner.com>
2018-12-05 17:17:47 +1100

    TTImport Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Entity/TTImport.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 5/12/2018
+ * Time: 17:13
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class TTImport
+ * @package App\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\TTImportRepository")
+ * @ORM\Table(name="TTImport")
+ */
+class TTImport
+{
+    /**
+     * @var integer|null
+     * @ORM\Id
+     * @ORM\Column(type="bigint", name="gibbonTTImportID", columnDefinition="INT(14) UNSIGNED ZEROFILL")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=6, name="courseNameShort")
+     */
+    private $courseNameShort;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=5, name="classNameShort")
+     */
+    private $classNameShort;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=12, name="dayName")
+     */
+    private $dayName;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=12, name="rowName")
+     */
+    private $rowName;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text", name="teacherUsernameList")
+     */
+    private $teacherUsernameList;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=30, name="spaceName")
+     */
+    private $spaceName;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return TTImport
+     */
+    public function setId(?int $id): TTImport
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCourseNameShort(): ?string
+    {
+        return $this->courseNameShort;
+    }
+
+    /**
+     * @param string|null $courseNameShort
+     * @return TTImport
+     */
+    public function setCourseNameShort(?string $courseNameShort): TTImport
+    {
+        $this->courseNameShort = $courseNameShort;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClassNameShort(): ?string
+    {
+        return $this->classNameShort;
+    }
+
+    /**
+     * @param string|null $classNameShort
+     * @return TTImport
+     */
+    public function setClassNameShort(?string $classNameShort): TTImport
+    {
+        $this->classNameShort = $classNameShort;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDayName(): ?string
+    {
+        return $this->dayName;
+    }
+
+    /**
+     * @param string|null $dayName
+     * @return TTImport
+     */
+    public function setDayName(?string $dayName): TTImport
+    {
+        $this->dayName = $dayName;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRowName(): ?string
+    {
+        return $this->rowName;
+    }
+
+    /**
+     * @param string|null $rowName
+     * @return TTImport
+     */
+    public function setRowName(?string $rowName): TTImport
+    {
+        $this->rowName = $rowName;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTeacherUsernameList(): ?string
+    {
+        return $this->teacherUsernameList;
+    }
+
+    /**
+     * @param string|null $teacherUsernameList
+     * @return TTImport
+     */
+    public function setTeacherUsernameList(?string $teacherUsernameList): TTImport
+    {
+        $this->teacherUsernameList = $teacherUsernameList;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSpaceName(): ?string
+    {
+        return $this->spaceName;
+    }
+
+    /**
+     * @param string|null $spaceName
+     * @return TTImport
+     */
+    public function setSpaceName(?string $spaceName): TTImport
+    {
+        $this->spaceName = $spaceName;
+        return $this;
+    }
+}
