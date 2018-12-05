6c9e7646a733dc4431c8b800a0564eea0ad0941c
Craig Rayner <craig@craigrayner.com>
2018-12-05 22:34:10 +1100

    YearGroup Entity Added
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Entity/YearGroup.php
+++ src/Entity/YearGroup.php
@@ -27,7 +27,6 @@
  * Date: 23/11/2018
  * Time: 15:27
  */
-
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -36,7 +35,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Class YearGroup
  * @package App\Entity
  * @ORM\Entity(repositoryClass="App\Repository\YearGroupRepository")
- * @ORM\Table(name="YearGroup")
+ * @ORM\Table(name="YearGroup", uniqueConstraints={@ORM\UniqueConstraint(name="name", columns={"name","nameShort","sequenceNumber"})})
  */
 class YearGroup
 {
@@ -47,4 +46,119 @@ class YearGroup
      * @ORM\GeneratedValue
      */
     private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=10)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @ORM\Column(length=4, name="nameShort")
+     */
+    private $nameShort;
+
+    /**
+     * @var integer
+     * @ORM\Column(type="smallint",columnDefinition="INT(3)",name="sequenceNumber")
+     */
+    private $sequenceNumber;
+
+    /**
+     * @var Person|null
+     * @ORM\ManyToOne(targetEntity="Person")
+     * @ORM\JoinColumn(name="gibbonPersonIDHOY",referencedColumnName="gibbonPersonID")
+     */
+    private $headOfYear;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return YearGroup
+     */
+    public function setId(?int $id): YearGroup
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return YearGroup
+     */
+    public function setName(?string $name): YearGroup
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNameShort(): ?string
+    {
+        return $this->nameShort;
+    }
+
+    /**
+     * @param string|null $nameShort
+     * @return YearGroup
+     */
+    public function setNameShort(?string $nameShort): YearGroup
+    {
+        $this->nameShort = $nameShort;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSequenceNumber(): int
+    {
+        return $this->sequenceNumber;
+    }
+
+    /**
+     * @param int $sequenceNumber
+     * @return YearGroup
+     */
+    public function setSequenceNumber(int $sequenceNumber): YearGroup
+    {
+        $this->sequenceNumber = $sequenceNumber;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getHeadOfYear(): ?Person
+    {
+        return $this->headOfYear;
+    }
+
+    /**
+     * @param Person|null $headOfYear
+     * @return YearGroup
+     */
+    public function setHeadOfYear(?Person $headOfYear): YearGroup
+    {
+        $this->headOfYear = $headOfYear;
+        return $this;
+    }
 }
