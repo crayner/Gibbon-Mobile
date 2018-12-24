be92f30dcce30300516685a98154bb724dc5aafa
Craig Rayner <craig@craigrayner.com>
2018-12-24 16:24:10 +1100

    Add Logger DB Handler
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Logger/MonologDBHandler.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in the LICENCE file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Gibbon-Mobile
+ *
+ * (c) 2018 Craig Rayner <craig@craigrayner.com>
+ *
+ * User: craig
+ * Date: 24/12/2018
+ * Time: 16:14
+ */
+namespace App\Logger;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+/**
+ * Class MonologDBHandler
+ * @package App\Logger
+ */
+class MonologDBHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    /**
+     * MonologDBHandler constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct(Logger::DEBUG, false);
+        $this->em = $em;
+    }
+
+    /**
+     * Called when writing to our database
+     * @param array $record
+     */
+    protected function write(array $record)
+    {
+        var_dump($record);
+ /*       $logEntry = new Log();
+        $logEntry->setMessage($record['message']);
+        $logEntry->setLevel($record['level']);
+        $logEntry->setLevelName($record['level_name']);
+        $logEntry->setExtra($record['extra']);
+        $logEntry->setContext($record['context']);
+
+        $this->em->persist($logEntry);
+        $this->em->flush();
+ */
+    }
+}
