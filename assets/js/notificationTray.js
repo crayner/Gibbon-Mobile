09078768d861f147634973c537bc57248ab0c8e8
Craig Rayner <craig@craigrayner.com>
2019-01-05 10:11:09 +1100

    Tweak Display sizes
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- assets/js/notificationTray.js
+++ assets/js/notificationTray.js
@@ -4,9 +4,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import TrayApp from './NotificationTray/TrayApp'
 
-render(
-    <TrayApp
-        {...window.TRAY_PROPS}
-    />,
-    document.getElementById('notificationTray')
-)
+const tray = document.getElementById('notificationTray')
+if (tray === null)
+    render(<div>&nbsp;</div>, document.getElementById('dumpStuff') )
+else
+    render(
+        <TrayApp
+            {...window.TRAY_PROPS}
+        />,
+        tray
+    )
