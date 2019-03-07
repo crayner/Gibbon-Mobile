e4e0397b6d7451b1ae60b8504b0c6b3f8a910675
Craig Rayner <craig@craigrayner.com>
2019-03-07 12:13:14 +1100

    PHPUnit Initial Tests XXVIII
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Migrations/2019/03/Version20190306095100.php
+++ src/Migrations/2019/03/Version20190306095100.php
@@ -42,7 +42,6 @@ final class Version20190306095100 extends SqlLoad
 {
     public function up(Schema $schema) : void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->getSql('Gibbon-v17.sql');
         parent::up($schema);
@@ -51,10 +50,13 @@ final class Version20190306095100 extends SqlLoad
         $cuttingEdge = count($sql);
         parent::up($schema);
 
-        $this->addSql("UPDATE `gibbonSetting` SET `value` = '".strval($cuttingEdge ?: '')."' WHERE `scope` = 'System' AND `name` = 'cuttingEdgeCodeLine'");
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("UPDATE `gibbonSetting` SET `value` = \'".strval($cuttingEdge ?: '')."\' WHERE `scope` = \'System\' AND `name` = \'cuttingEdgeCodeLine\'");
 
-        $this->addSql("UPDATE `gibbonSetting` SET `value` = '18.0.00') WHERE `scope` = 'System' AND `name` = 'version'");
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("UPDATE `gibbonSetting` SET `value` = \'18.0.00\' WHERE `scope` = \'System\' AND `name` = \'version\'");
 
-        $this->addSql("UPDATE `gibbonSetting` SET `value` = 'Y' WHERE `scope` = \'System\' AND `name` = \'cuttingEdgeCode\'");
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("UPDATE `gibbonSetting` SET `value` = \'Y\' WHERE `scope` = \'System\' AND `name` = \'cuttingEdgeCode\'");
     }
 }
